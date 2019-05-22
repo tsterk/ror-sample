ef27ca71c195fb867f2aafa813b62a49401ff2e7
Tim Sterk <tim.sterk@npo.nl>
2019-05-22 14:52:50 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 cp -Rv /tmp/src/* /var/www/html
 
 # Ugly
-mkdir -p /var/www/html/tmp/cache
-chmod 777 /var/www/html/tmp/cache
+chmod -v 775 /var/www/html/tmp /var/www/html/log
 
 cd /var/www/html; bundle install
