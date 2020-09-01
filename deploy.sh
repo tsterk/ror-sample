ddc54156db9e0f862ac110c1f13a2718a0895d4d
Tim Sterk <tim.sterk@npo.nl>
2020-09-01 09:50:15 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ cp -Rv /tmp/src/* /var/www/html
 
 # Run bundle install only when running in a ruby container.
 if [ -f /usr/local/bin/bundle ]; then
-	cd /var/www/html; bundle install
+	cd /var/www/html; bundle install --no-deployment
 	chmod -v 775 /var/www/html/tmp /var/www/html/log
 fi
